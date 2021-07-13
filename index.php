83c5cb0aa1455064f1f18f54b8d9750534852ded
Gottschalk16 <raianne_gottschalk@hotmail.com.br>
2021-07-12 22:03:25 -0300

    Adicionado Bootstrap ao projeto e inserido os "link" e "script" no index

--- index.php
+++ index.php
@@ -1,14 +1,19 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset='utf-8'>
-    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>Inicio</title>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
-    <script src='main.js'></script>
+   <meta charset='utf-8'>
+   <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+   <title>Inicio</title>
+   <meta name='viewport' content='width=device-width, initial-scale=1'>
+   <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+   <link rel="stylesheet" href="css/bootstrap.min.css">
+   <script src='main.js'></script>
 </head>
 <body>
-    
+
 </body>
+<script src="js/jquery-3.3.1.slim.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.hoverdir.js"></script>	
 </html>
