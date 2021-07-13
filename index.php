628dcf0e3102acf330c7a3050f089574543166c0
Gottschalk16 <raianne_gottschalk@hotmail.com.br>
2021-07-12 22:40:40 -0300

    Adicionado navbar inicial

--- index.php
+++ index.php
@@ -10,7 +10,21 @@
    <script src='main.js'></script>
 </head>
 <body>
-
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+   <div class="container-fluid">
+      <!--Adicionar ícone -->
+      <img src="" alt="" width="30" height="24">
+      <div class="nav justify-content-end" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+         <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Início</a>
+         </li>
+         <li class="nav-item">
+            <a class="nav-link" href="#">#</a>
+         </li>      
+      </ul>    
+   </div>
+</nav>
 </body>
 <script src="js/jquery-3.3.1.slim.min.js"></script>
 <script src="js/popper.min.js"></script>
