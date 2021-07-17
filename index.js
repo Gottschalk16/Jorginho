82f993878a57bebefeb9da4da411294929d4316b
Gottschalk16 <raianne_gottschalk@hotmail.com.br>
2021-07-16 22:08:10 -0300

    Adicionado nodefetch e condição para buscar gif's

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 console.log('Beep beep! :)');
+const fetch = require('node-fetch');
 //cria o client que faz a ligação com o discord
 const Discord = require('discord.js');
 const client = new Discord.Client();
@@ -11,11 +12,24 @@ client.on('message', getMessage);
 function readyDiscord(){
    console.log('I will do everything you want, but you will be charged!');
 }
-//Captura as mensagens enviadas nos canais do servidor
-function getMessage(msg){
+//Captura as mensagens enviadas nos canais do servidor e realiza ações de acordo com o comando
+async function getMessage(msg){
+   let tokens = msg.content.split(" ");
    console.log(msg.content);
    //Se a mensagem for igual a gostosa então escreve gostosa no chat
-   if (msg.content === 'gostosa'){
+   if (tokens [0] === 'gostosa'){
       msg.reply('gostosa');
+   }
+   if (tokens [0] === '!gif'){                 
+      let keywords = 'sasuke';
+      if (tokens.length > 1){
+         keywords = tokens.slice(1, tokens.length).join("");
+      }
+      let url = `https://api.tenor.com/v1/search?q=&{keywords}&key=&{process.env.TokenTenor}&contentfillter=high`;
+      let response = await fetch(url);
+      let json = await response.json();
+      const index = Math.floor(Math.random() * json.results.length);
+      console.log(json);
+      msg.channel.send(json.results[index].url);
    }
 }
